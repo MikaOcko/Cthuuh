8a593beb20adefb6749b04cdf8fec052165248bd
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-08-21 17:21:55 +0200

    modification du layout guest (inscription, connexion)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,18 +7,22 @@
 
         <title>{{ config('app.name', 'Cthuuh') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+         <!-- google fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <!-- Big Shoulder display -->
+        <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+            <!-- Fira sans -->
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <body class="font-sans text-black antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-black">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="{{ url('/')}}">
+                    <img src="../images/logo-texte.png" alt="logo texte de Cthuuh" class="block h-16 w-auto">
                 </a>
             </div>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ url('/home')}}">
+                    <a href="{{ url('/')}}">
                         <img src="../images/logo-texte.png" alt="logo texte de Cthuuh" class="block h-9 w-auto">
                     </a>
                 </div>
@@ -80,17 +80,47 @@
                             </form>
                         </x-slot>
                     </x-dropdown>
+
+                    <div class="flex justify-between items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                        </svg>
+                          
+                        <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                            {{ __('S\'inscrire') }}
+                        </x-nav-link>
+
+                        {{-- <x-nav-link>
+                            {{ __('Mon apnier') }}
+                        </x-nav-link> --}}
+                    </div>
+                    
                 </div>
 
                 @else
-                
-                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                        {{ __('S\'inscrire') }}
-                    </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <div class="flex justify-between items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                        </svg>
+                          
+                        <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                            {{ __('S\'inscrire') }}
+                        </x-nav-link>
+                    </div>
                     
-                    <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                        {{ __('Se connecter') }}
-                    </x-nav-link>
+                    <div class="flex justify-between items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                        </svg>
+             
+                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                            {{ __('Se connecter') }}
+                        </x-nav-link>
+                    </div>
+                    
+                </div>
+                    
                 @endauth
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
@@ -146,6 +176,9 @@
         </div>
 
         @else
+        <div>
+            
+        </div>
             <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
                 {{ __('S\'inscrire') }}
             </x-responsive-nav-link>
