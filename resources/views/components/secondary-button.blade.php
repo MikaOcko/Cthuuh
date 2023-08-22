b1dced72f722caf6c325b68376d6c518158e99d5
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-08-22 14:43:49 +0200

    ajustement graphique du composant secondary-button + début de mise en forme (hero) de la page shop

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'py-2 px-4 bg-transparent text-white font-semibold border-2 border-white rounded hover:bg-white hover:text-transaprent hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'py-2 px-4 bg-transparent text-white font-semibold border-2 border-white rounded hover:bg-white hover:text-black hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0']) }}>
     {{ $slot }}
 </button>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,7 +1,24 @@
 <x-app-layout>
     <x-slot name="hero">
-        <h1>Bienvenue dans le monde de Cthuuh, où l'inimaginable devient réalité.</h1>
-        <p>Nous vous proposons une gamme de boissons énergisantes qui éveilleront vos sens et vous transporteront dans un royaume au-delà de la compréhension humaine.</p>
+        <div class="w-full h-full bg-gradient-to-r from-orange-100 to-orange-400 p-10">
+        {{-- <div class="w-full h-full bg-hero"> --}}
+            <div class="container flex flex-col px-6 py-10 mx-auto space-y-6">
+                <div class="w-full">
+                    <div>
+                        <h1>
+                            Bienvenue dans le monde de Cthuuh, où l'inimaginable devient réalité.
+                        </h1>
+                            
+                        <div class="mt-8 space-y-5">
+                             <p>
+                                Nous vous proposons une gamme de boissons énergisantes qui éveilleront vos sens et vous transporteront
+                                dans un royaume au-delà de la compréhension humaine.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </x-slot>
 
     <x-slot name="content">
